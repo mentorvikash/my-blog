56b901e09ef8383daa7c6fb590ad228749131219
mentorvikash <mentorvikash@gmail.com>
2023-10-23 07:10:56 +0530

    change .js to .tsx

---
+++ pages/[slug].tsx
@@ -0,0 +1,38 @@
+// pages/[slug].js
+import { useRouter } from "next/router";
+import { getBlogPostBySlug, getAllBlogPostSlugs } from "../lib/blogPosts";
+
+const BlogPost = ({ post }: any) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div className="text-center mt-8">Loading...</div>;
+  }
+
+  return (
+    <div className="max-w-screen-lg mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <div
+        className="prose prose-blue"
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  const paths = getAllBlogPostSlugs();
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const post = getBlogPostBySlug(params.slug);
+  return {
+    props: { post },
+  };
+}
+
+export default BlogPost;
---
+++ pages/admin/index.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Admin = () => {
+  // Fetch and list your existing blog posts here
+  // You can use API Routes to fetch the data from your backend
+
+  return (
+    <div className="max-w-screen-lg mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Admin Panel</h1>
+      <Link href="/admin/create" className="text-blue-500 hover:underline">
+        Create New Post
+      </Link>
+      {/* List of existing posts */}
+    </div>
+  );
+};
+
+export default Admin;
